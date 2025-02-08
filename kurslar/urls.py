212eeeccac61c0b7dc502d394788ba537dc51f48
samehmet00 <151998236+samehmet00@users.noreply.github.com>
2025-02-08 16:32:02 +0300

    İlk commit

---
+++ kurslar/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+# http://127.0.0.1:8000/        => ANA SAYFA
+# http://127.0.0.1:8000/home    => ANA SAYFA
+# http://127.0.0.1:8000/kurslar => kurs listesi
+
+urlpatterns = [
+    path("",views.kurslar),
+    path("list",views.kurslar),
+    path("<kurs_adi>",views.details),
+    path("kategori/<int:category_id>",views.getCoursesByCategoryId),
+    path("kategori/<str:category_name>",views.getCoursesByCategory, name="courses_by_category"),
+]
